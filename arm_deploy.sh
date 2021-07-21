e0513d4e2a7a62e1003146d613eff4d543ad54a4
Bree Stryker <b-s-no-reply@microsoft.com>
2021-07-21 09:57:23 -0400

    Tenant ID

--- arm_deploy.sh
+++ arm_deploy.sh
@@ -29,7 +29,7 @@ cd ./src/scripts
 az login --service-principal \
 --user "${env_service_prin}" \
 --password="${env_service_prin_secret}" \
---tenant "$(az account show --query "tenantId" --output tsv)" \
+--tenant "${$env_tenant_id}" \
 --allow-no-subscriptions \
 --output json
 
